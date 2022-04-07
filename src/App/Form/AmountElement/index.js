df52929c71690424370f4a2c46c67c386a912082
Sebastian Komorowski <sebastiano.komo@hotmail.com>
2022-04-07 22:02:43 +0200

    AmountElement component

--- src/App/Form/AmountElement/index.js
+++ src/App/Form/AmountElement/index.js
@@ -1,5 +1,22 @@
 const AmountElement = () => {
 
+    return (
+        <p>
+            <label 
+                className="form__label">
+                <span 
+                    className="form__labelText">
+                        Amount to change (PLN):
+                </span>
+                <input 
+                    type="number" 
+                    step="0.01" 
+                    min="1" 
+                    className="form__input" 
+                    required/>
+            </label>
+        </p>
+    )
 };
 
 export {AmountElement};
