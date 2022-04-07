41dd7a731c3a52ef7fdc98826fbf186404e3c53c
Sebastian Komorowski <sebastiano.komo@hotmail.com>
2022-04-07 21:38:14 +0200

    Added legend and fieldset

--- src/App/Form/index.js
+++ src/App/Form/index.js
@@ -1,5 +1,17 @@
 const Form = () => {
 
+    return (
+        <form 
+            className="form">
+            <fieldset 
+                className="form__fieldset">
+                <legend 
+                    className="form__legend">
+                        Currency converter 😉
+                </legend>
+            </fieldset>
+        </form>
+    )
 };
 
 export {Form};
