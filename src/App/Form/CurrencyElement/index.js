82a7349bb6ae12de6c94db0d7022a8a7429e51cd
Sebastian Komorowski <sebastiano.komo@hotmail.com>
2022-04-07 21:48:04 +0200

    CurrencyElement

--- src/App/Form/CurrencyElement/index.js
+++ src/App/Form/CurrencyElement/index.js
@@ -1,5 +1,20 @@
 const CurrencyElement = () => {
 
+    return (
+        <p>
+            <label 
+                className="form__label">
+                <span 
+                    className="form__labelText">
+                        Currency:
+                </span>
+                <select 
+                    className="form__input">
+                    
+                </select>
+            </label>
+        </p>
+    );
 };
 
 export {CurrencyElement};
