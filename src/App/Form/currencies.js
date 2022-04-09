394b8baddfde93838a60dfcc010eb4eeda7b64aa
Sebastian Komorowski <sebastiano.komo@hotmail.com>
2022-04-09 18:39:32 +0200

    Added currencies.js

---
+++ src/App/Form/currencies.js
@@ -0,0 +1,17 @@
+export const currencies = [
+    {
+        short: "USD",
+        name: "U.S. Dollar",
+        rate: 4.26,
+    },
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.63,
+    },
+    {
+        short: "GBP",
+        name: "British Pound",
+        rate: 4.26,
+    },
+];
