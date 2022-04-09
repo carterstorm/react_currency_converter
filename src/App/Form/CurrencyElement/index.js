b4d75c0f5831891635cec4b1e9bcafa279ca4447
Sebastian Komorowski <sebastiano.komo@hotmail.com>
2022-04-09 19:41:15 +0200

    Added currencies values to select element

--- src/App/Form/CurrencyElement/index.js
+++ src/App/Form/CurrencyElement/index.js
@@ -1,4 +1,8 @@
+import { useState } from "react";
+import { currencies } from "../currencies";
+
 const CurrencyElement = () => {
+    const [currency, setCurrency] = useState(currencies[0].short);
 
     return (
         <p>
@@ -9,8 +13,19 @@ const CurrencyElement = () => {
                         Currency:
                 </span>
                 <select 
-                    className="form__input">
-                    
+                    className="form__input"
+                    value={currency}
+                    onChange={({target}) => setCurrency(target.value)}
+                    >
+                        {currencies.map((item) => (
+                            <option
+                                key={item.short}
+                                value={item.short}
+                                >
+                                {item.short}
+                            </option>
+                            ))
+                        }
                 </select>
             </label>
         </p>
