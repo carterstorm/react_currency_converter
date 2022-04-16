d7922619399addd9ebd9da6a527c08b30608aade
Sebastian Komorowski <sebastiano.komo@hotmail.com>
2022-04-16 14:05:46 +0200

    added clearInterval

--- src/App/Form/Clock/index.js
+++ src/App/Form/Clock/index.js
@@ -6,9 +6,13 @@ const Clock = () => {
     const [date, setDate] = useState(new Date());
 
     useEffect(() => {
-        setInterval(() => {
+        const intervalId = setInterval(() => {
             setDate(new Date());
-        }, 1000)
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
     }, []);
 
     return (
