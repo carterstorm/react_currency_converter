be46bc313a23c19b2b696a8195fc750776fc1518
Sebastian Komorowski <sebastiano.komo@hotmail.com>
2022-05-01 14:01:58 +0200

    Added useDate hook

--- src/App/Form/Clock/index.js
+++ src/App/Form/Clock/index.js
@@ -1,19 +1,9 @@
-import { useEffect, useState } from "react";
 import { ClockElement } from "./styled";
+import { useDate } from "../../../useDate";
 
 const Clock = () => {
 
-    const [date, setDate] = useState(new Date());
-
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setDate(new Date());
-        }, 1000);
-
-        return () => {
-            clearInterval(intervalId);
-        };
-    }, []);
+    const date = useDate();
 
     return (
         <ClockElement>
