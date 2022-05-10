982bac1ce862ca4b02a08732c42e93c04f1f5354
Sebastian Komorowski <sebastiano.komo@hotmail.com>
2022-05-10 22:06:11 +0200

    created useRates Hook

--- src/useRates.js
+++ src/useRates.js
@@ -1,7 +1,24 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-export const useRates = () => {
-    const [rates, setRates] = useState({
+export const useApiRates = () => {
+    const [apiRates, setApiRates] = useState({
         state: "Loading...",
     });
+
+    useEffect(() => {
+        const getRates = async () => {
+            try {
+                const response = await axios.get("https://api.exchangerate.host/latest?base=PLN");
+                const {rate, date} = response.data;
+
+            } catch(error) {
+                console.error(error);
+            };
+        };
+
+        setTimeout(getRates, 1*1000);
+    }, []);
+
+    return apiRates;
 };
